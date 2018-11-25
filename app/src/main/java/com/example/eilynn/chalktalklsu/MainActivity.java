745269cba120e7444879788018cc36ce46b55d93
carolakey <carol.key42@gmail.com>
2018-11-25 11:23:47 -0600

    Updated MainActivity

--- app/src/main/java/com/example/eilynn/chalktalklsu/MainActivity.java
+++ app/src/main/java/com/example/eilynn/chalktalklsu/MainActivity.java
@@ -8,11 +8,26 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button login;
+    private Button reg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        login = findViewById(R.id.logbutton);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+        reg = findViewById(R.id.regButton);
+        reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+            }
+        });
     }
-}
+    }
