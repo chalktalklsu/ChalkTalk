773651a1cd24b1f67359d00d37d64e3aff456658
cyborgQT <38447542+cyborgQT@users.noreply.github.com>
2018-11-25 21:55:24 -0600

    Added PostActivity

--- app/src/main/java/com/example/eilynn/chalktalklsu/MainActivity.java
+++ app/src/main/java/com/example/eilynn/chalktalklsu/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.eilynn.chalktalklsu;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -92,10 +93,18 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_send) {
 
+        } else if(id == R.id.create_post){
+            SendUserToPostActivity();
+
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void SendUserToPostActivity() {
+        Intent newPost = new Intent(MainActivity.this, PostActivity.class);
+        startActivity(newPost);
     }
 }
